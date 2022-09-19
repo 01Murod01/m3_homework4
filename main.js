116733b3a411b6420912e032763f1d68dc971fb7
01Murod01 <01myoldoshev01@gmail.com>
2022-09-19 19:09:30 +0500

    lesson4

---
+++ main.js
@@ -0,0 +1,82 @@
+"use strict";
+
+// 1 misol
+// let k = prompt("k ni kiriting:");
+// let n = prompt("n ni kiriting:");
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+// 2 misol
+// let a = Number(prompt("a ni kiriting:"));
+// let b = Number(prompt("b ni kiriting:"));
+// for (let i = b; i <= a; i++){
+//   console.log(i);
+// }
+
+// 3 misol
+// let a = Number(prompt("a ni kiring:"));
+// let b = Number(prompt("b ni kiring:"));
+// for (let i = a; i > b; i--){
+//     console.log(i)
+// }
+
+// 4 misol
+// let k = 10000;
+// for (let i = 1; i <= 10; i++) {
+// console.log(i+"kg", i*k+"so'm")
+// }
+
+// 5 misol
+// let k = 10000;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i * 0.1 + "kg", i * k * 0.1 + "so'm");
+// }
+// 6 misol
+// let k = 10000;
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//   console.log(i + "kg", Math.ceil(i * k) + "so'm");
+// }
+
+// 7 misol
+// const a = Number(prompt("a ni kiriting:"));
+// const b = Number(prompt("b ni kiriting:"));
+// let counter = 0;
+// for (let i = a; i <= b; i++) {
+//   counter = counter + i;
+// }
+// console.log(counter);
+
+// 8 misol
+// const a = Number(prompt("a ni kiriting:"));
+// const b = Number(prompt("b ni kiriting:"));
+// let counter = 1;
+// for (let i = a; i <= b; i++) {
+//   counter = counter * i;
+// }
+// console.log(counter);
+
+// 9 misol oxwamadi
+// let a = Number(prompt("a ni kiriting:"));
+// let b = Number(prompt("b ni kiriting:"));
+// let counter = 1;
+// for (let i = a; i <= b; i++) {
+//   counter = counter * i;
+// }
+// console.log(counter);
+
+// 10 misol oxwamadi
+// const n = Number(prompt("n ni kiriting:"));
+// const k = 1;
+// let counter = 0;
+// for (let i = (1/i); i < (k / n); i++) {
+// counter = counter + i;
+// console.log(i);
+// }
+
+// 11 misol
+// let n = 20;
+// for (let i = 0; i){
+
+// }
+
